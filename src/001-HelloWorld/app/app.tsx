16d888fefaa06d8efe67a557d49cf719c8371ee6
BoyceLyu <lvzhendong.bl@gmail.com>
2016-02-22 14:33:12 +0800

    add demo1 helloworld

---
+++ src/001-HelloWorld/app/app.tsx
@@ -0,0 +1,20 @@
+/// <reference path="./interfaces/interfaces.d.ts" />
+
+import * as React from "react";
+import {render} from "react-dom";
+
+/**
+ * HelloWorld
+ */
+class HelloWorld extends React.Component<any,any> {
+      render(){
+          return <div>Hello World!</div>;
+        }
+    }
+    
+    
+render(
+    <HelloWorld />,
+    document.getElementById("root")
+)
+
